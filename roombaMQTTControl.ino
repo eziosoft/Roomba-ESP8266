4e34f74e2ce59eb3f4c7006c266e1ebcb0b2a00a
ezio <eziotest@gmail.com>
2020-11-13 21:10:29 +0100

    first

---
+++ roombaMQTTControl.ino
@@ -0,0 +1,231 @@
+
+#include "roombaDefines.h"
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <WiFiManager.h>
+#include <PubSubClient.h>
+#include <ArduinoOTA.h>
+#include <SoftwareSerial.h>
+
+
+const char* ssid = "Robot"; //robot creates wifi hotspot when wifi connection is not configured
+const char* outTopic = "tank/out"; //MQTT topic for robot telemetry messages
+const char* inTopic = "tank/in"; //MQTT topic for control messages
+const char* mqtt_server = "192.168.0.19"; //my defauld MQTT server
+const char* mqtt_server1 = "test.mosquitto.org";
+
+
+SoftwareSerial Roomba(D7, D6); //rx, tx
+
+
+char buffer1[20]; //multiusage
+WiFiClient espClient;
+PubSubClient client(espClient); //MQTT
+
+
+void setup() {
+  pinMode(D5, OUTPUT);
+  Serial.begin(115200);
+  Roomba.begin(115200);
+
+  wakeUp();
+  startSafe();
+  playSound (2);
+  stop();
+  delay(2000);
+
+  startSafe();
+  playSound (2);
+
+  setDebrisLED(ON);
+  writeLEDs ('W', 'I', 'F', 'Y');
+
+  WiFiManager wifiManager;
+  wifiManager.autoConnect("Robot");
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+
+  //OTA
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH)
+      type = "sketch";
+    else // U_SPIFFS
+      type = "filesystem";
+
+    // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+    Serial.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
+  //OTA END
+
+
+  ////////////////////
+
+  playSound (3);
+  setPowerLED(255, 255);
+  setDebrisLED(ON);
+  setDockLED(OFF);
+  setSpotLED(OFF);
+  setWarningLED(OFF);
+  writeLEDs ('R', 'E', 'D', 'Y');
+
+  //motorSquareTest(); // un-comment if you want to test Roomba doing a square
+  //  turnCW (40, 180);  //un-comment if you want to test Roomba spin clock-wise 180 degrees and stop
+  // driveWheels(20, 10); //un-comment if you want to test Roomba spin
+  //  driveLeft(20); //un-comment if you want to test Roomba turning left
+
+
+  Serial.print("READY");
+}
+
+void loop() {
+  //  if (Serial.available())
+  //  {
+  //    command = Serial.read();
+  //    Serial.flush();
+  //    manualCmd ();
+  //  }
+
+
+  ArduinoOTA.handle();
+
+  //MQTT
+  if (!client.connected()) reconnect();
+  else client.loop();
+
+  //send telemetry every 200ms
+  if (millis() % 1000 == 0) {
+    int voltage =  0;//getSensorData(22);
+    Serial.println(voltage);
+
+    sprintf(buffer1, "T;%d;RSSI=%d;%d", millis() / 1000, WiFi.RSSI(), voltage);
+    client.publish(outTopic, buffer1);
+  }
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  //control is done by 6 byte messages : [$][len][ch1][ch2][ch3][ch4]
+  // [$] - start of the frame
+  // [len] - number of bytes to read. in this case always 4
+  // [ch1][ch2][ch3][ch4] - control channels. Each channel has a range from 0 to 200. Middle is at 100 - servo stop
+  if (length > 0) {
+    if ((char)payload[0] == '$') {
+
+      int len = payload[1];
+      int ch1 = payload[2] - 100;
+      int ch2 = payload[3] - 100;
+      int ch3 = payload[4] - 100;
+      int ch4 = payload[5] - 100;
+
+
+      //chanel mixer
+      int m1 = ch2 - ch1;
+      int m2 = ch2 + ch1;
+      if (m1 > 100)m1 = 100;
+      if (m1 < -100) m1 = -100;
+      if (m2 > 100)m2 = 100;
+      if (m2 < -100) m2 = -100;
+
+      m1 = map(m1, -100, 100, -500, 500);
+      m2 = map(m2, -100, 100, -500, 500);
+      driveWheels(m2, m1);
+
+      int m3 = map(ch3, -100, 100, -100, 100);
+      int m4 = map(ch4, -100, 100, -100, 100);
+
+      switch (ch3)
+      {
+        case 1:
+          stop();
+          //powerOff();
+          break;
+
+        case 2:
+          wakeUp();
+          startFull();
+          playSound (2);
+          setPowerLED(255, 255);
+          setDebrisLED(ON);
+          setDockLED(OFF);
+          setSpotLED(OFF);
+          setWarningLED(OFF);
+          writeLEDs ('R', 'E', 'D', 'Y');
+          break;
+
+        case 3:
+          seekDock();
+          break;
+
+        case 10:
+          enableBrushes(true, false, true, true, true);
+          break;
+
+        case 11:
+          enableBrushes(true, true, false, false, false);
+          break;
+
+        case 12:
+          writeLEDs ('-', 'G', 'O', '-');
+          clean();
+          break;
+
+
+
+      }
+
+      //debug
+      Serial.print(ch1);
+      Serial.print(" ");
+      Serial.print(ch2);
+      Serial.print("->");
+      Serial.print(ch3);
+      Serial.print(" ");
+      Serial.println(ch4);
+    }
+  }
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect(ssid)) {
+      Serial.println("connected");
+      playSound (1);
+      // Once connected, publish an announcement...
+      client.publish(outTopic, "Tank READY");
+      // ... and resubscribe
+      client.subscribe(inTopic);
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
